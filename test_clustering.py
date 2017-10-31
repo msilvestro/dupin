2292d8926f6ef713020c9f226127d1aebdca63ff
Matteo Silvestro <matteosilvestro@live.it>
2017-10-31 19:43:34 +0100

    Changed the clustering tests to be handled more easily

--- test_clustering.py
+++ test_clustering.py
@@ -1,68 +1,60 @@
-"""Hierarchical clustering for DoppioGioco."""
+"""Clustering for DoppioGioco."""
 # pylint: disable=C0103
 from time import time
 import numpy as np
-from sklearn.cluster import AgglomerativeClustering, KMeans
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import silhouette_score
 from clustering.kmedoids import pam_npass
-from clustering.metrics import (_dissimilarity_matrix, euclidean_distance,
-                                manhattan_distance, supremum_distance)
+from clustering.metrics import (dissimilarity_matrix, _dissimilarity_matrix,
+                                euclidean_distance, manhattan_distance,
+                                supremum_distance)
+from scipy.spatial.distance import cityblock
+from fastdtw import fastdtw
 
 K_MIN = 2
-K_MAX = 50
-# METHOD = "agglomerative_clustering_ed"
-# METHOD = "agglomerative_clustering_md"
-# METHOD = "agglomerative_clustering_sd"
-METHOD = "agglomerative_clustering_cosine"
-# METHOD = "kmeans"
-# METHOD = "kmedoids_ed"
-# METHOD = "kmedoids_md"
+K_MAX = 51
+METHOD = 'kmedoids'
+DISTANCE = 'dtw'
+DEBUG = False
 
-data = np.loadtxt('data.gz')
+data = np.loadtxt('data/curves_exp_zeros.gz')
+if DEBUG:
+    np.random.shuffle(data)
+    data = data[:100]
 
 k_range = np.arange(K_MIN, K_MAX)
 sil_scores = np.zeros(K_MAX - K_MIN)
 all_labels = np.empty((K_MAX - K_MIN, data.shape[0]), dtype=np.uint32)
-if METHOD == "kmedoids_ed":
-    dissimilarity_matrix = _dissimilarity_matrix(euclidean_distance)
-    diss = dissimilarity_matrix(data)
-elif METHOD == "kmedoids_md":
-    dissimilarity_matrix = _dissimilarity_matrix(manhattan_distance)
-    diss = dissimilarity_matrix(data)
-elif METHOD == "agglomerative_clustering_sd":
-    dissimilarity_matrix = _dissimilarity_matrix(supremum_distance)
-    diss = dissimilarity_matrix(data)
+
+if DISTANCE == 'euclidean':
+    dm = _dissimilarity_matrix(euclidean_distance)
+    diss = dm(data)
+elif DISTANCE == 'manhattan':
+    dm = _dissimilarity_matrix(manhattan_distance)
+    diss = dm(data)
+elif DISTANCE == 'supremum':
+    dm = _dissimilarity_matrix(supremum_distance)
+    diss = dm(data)
+elif DISTANCE == 'dtw':
+    dtw = lambda x, y: fastdtw(x, y, dist=cityblock)[0]
+    diss = dissimilarity_matrix(data, dtw)
+
 for k in k_range:
     print("## {:} ##".format(k))
+
     start = time()
-    if METHOD == "agglomerative_clustering_ed":
-        labels = AgglomerativeClustering(k).fit_predict(data)
-    elif METHOD == "agglomerative_clustering_md":
-        labels = AgglomerativeClustering(k, affinity='manhattan',
-                                         linkage='average').fit_predict(data)
-    elif METHOD == "agglomerative_clustering_sd":
+    if METHOD == 'agglomerative_clustering':
         labels = AgglomerativeClustering(k, affinity='precomputed',
                                          linkage='average').fit_predict(diss)
-    elif METHOD == "agglomerative_clustering_cosine":
-        labels = AgglomerativeClustering(k, affinity='cosine',
-                                         linkage='average').fit_predict(data)
-    elif METHOD == "kmeans":
-        labels = KMeans(k).fit_predict(data)
-    elif METHOD in ("kmedoids_ed", "kmedoids_md"):
+    elif METHOD == 'kmedoids':
         labels = pam_npass(diss, k, npass=10)[0]
     print("Elapsed time: {:.4f}".format(time() - start))
-    if METHOD in ("agglomerative_clustering_ed", "kmeans", "kmedoids_ed"):
-        sil_score = silhouette_score(data, labels)
-    elif METHOD in ("agglomerative_clustering_md", "kmedoids_md"):
-        sil_score = silhouette_score(data, labels, metric='manhattan')
-    elif METHOD == "agglomerative_clustering_sd":
-        sil_score = silhouette_score(diss, labels, metric='precomputed')
-    elif METHOD == "agglomerative_clustering_cosine":
-        sil_score = silhouette_score(data, labels, metric='cosine')
+
+    sil_score = silhouette_score(diss, labels, metric='precomputed')
     print("Silhouette score: {:.6f}".format(sil_score))
     sil_scores[k - K_MIN] = sil_score
     all_labels[k - K_MIN] = labels
 
-np.savetxt('results/{:}_k_range.gz'.format(METHOD), k_range)
-np.savetxt('results/{:}_sil_scores.gz'.format(METHOD), sil_scores)
-np.savetxt('results/{:}_all_labels.gz'.format(METHOD), all_labels)
+np.savetxt('results/{:}_{:}_k_range.gz'.format(METHOD, DISTANCE), k_range)
+np.savetxt('results/{:}_{:}_sil_scores.gz'.format(METHOD, DISTANCE), sil_scores)
+np.savetxt('results/{:}_{:}_all_labels.gz'.format(METHOD, DISTANCE), all_labels)
